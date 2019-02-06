d33700fcf68920aec3b322788054a497fcfa4c90
Unknown <brandon@morrellworld.net>
2019-02-05 19:32:11 -0500

    Fixed Opt

---
+++ Pokemon Purple/Assets/CanvasScripts/CanvasManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public GameObject menu; 
+    private bool isShowing;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (menu.activeSelf)
+            {
+                menu.SetActive(false);
+            }
+            else
+            {
+                menu.SetActive(true);
+            }
+
+        }
+    }
+}
