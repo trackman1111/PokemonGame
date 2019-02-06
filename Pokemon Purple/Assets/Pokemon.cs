3e4da2185a0d9def12e134e2d4cfadbd59fe76de
Matthew Tekampe <matthew_tekampe@comcast.net>
2019-02-06 18:44:04 -0500

    gae

--- Pokemon Purple/Assets/Pokemon.cs
+++ Pokemon Purple/Assets/Pokemon.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Drawing;
 
 public class Pokemon : MonoBehaviour
 {
@@ -12,6 +14,7 @@ public class Pokemon : MonoBehaviour
     private int speed;
     private int level;
     private int exp;
+    public Sprite treecko;
     private IDictionary<string,double[]> pokemon;
     //private image image;
     // Start is called before the first frame update
@@ -19,11 +22,14 @@ public class Pokemon : MonoBehaviour
     {
         pokemon = new Dictionary<string,double[]>();
         // All arrays are in the format of [health, defence, attack, speed, level, exp]
+
+        //get pokemon number and an image for each pokemon
         double[] tree = {10.0, 15.0, 11.0, 30.0, 25.0, 32.0};
         pokemon.Add("Treecko", tree);
+          
 
         double[] grov = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
-        pokemon.Add("Grovyle", grov);
+        pokemon.Add("Grovyle", grov); 
 
         double[] scep = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
         pokemon.Add("Sceptile", scep);
@@ -37,6 +43,35 @@ public class Pokemon : MonoBehaviour
         double[] blaz = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
         pokemon.Add("Blaziken", blaz);
 
+        double[] mudk = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Mudkip", mudk);
+
+        double[] mars = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Marshtomp", mars);
+
+        double[] swam = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Swampert", swam);
+
+        double[] pooc = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Poochyena", pooc);
+
+        double[] migh = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Mightyena", migh);
+
+        double[] zigz = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Zigzagoon", zigz);
+
+        double[] lino = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Linoone", lino);
+
+        double[] kyog = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Kyogre", kyog);
+
+        double[] grou = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Groudon", grou);
+
+        double[] rayq = { 10.0, 15.0, 11.0, 30.0, 25.0, 32.0 };
+        pokemon.Add("Rayquaza", rayq);
 
     }
 
@@ -45,38 +80,41 @@ public class Pokemon : MonoBehaviour
     {
         
     }
-    void makeMoves(string type, int slot)
+    void makeMoves(string type)
     {
         if (type.Equals("Water"))
         {
-
-            pokemon[slot, 1] = "Tackle";
-            pokemon[slot, 2] = "Tail Whip";
-            pokemon[slot, 3] = "Bubble";
+            string[] waterTypeMoves = { "Tackle", "Water Gun", "Surf", "Hydro Pump" };
+            string[] waterSupportMoves = { "Defence Curl", "", "" };
         }
         else if (type.Equals("Fire"))
         {
-            pokemon[slot, 1] = "Scratch";
-            pokemon[slot, 2] = "Growl";
-            pokemon[slot, 3] = "Ember";
+            string[] fireTypeMoves = { "Scratch", "Ember", "Flamethrower", "Fire Blast" };
+            string[] fireSupportMoves = { "Growl", "", "" };
         }
         else if (type.Equals("Grass"))
         {
-            pokemon[slot, 1] = "Growl";
-            pokemon[slot, 2] = "Tackle";
-            pokemon[slot, 3] = "Vine Whip";
+            string[] grassTypeMoves = { "Peck", "Razor Leaf", "Gigadrain", "Solar Beam" };
+            string[] grassSupportMoves = { "", "", "" };
         }
         else if (type.Equals("Ground"))
         {
-            pokemon[slot, 1] = "Defense Curl";
-            pokemon[slot, 2] = "Scratch";
-            pokemon[slot, 3] = "Sand Attack";
+            string[] groundTypeMoves = { "Tackle", "Mud Slap", "Dig", "Earthquake" };
+            string[] groundSupportMoves = { "", "", "" };
         }
         else if (type.Equals("Electric"))
         {
-            pokemon[slot, 1] = "Growl";
-            pokemon[slot, 2] = "Tail Whip";
-            pokemon[slot, 3] = "Thunder Shock";
+            string[] electricTypeMoves = { "Quick Attack", "Thunderbolt", "Spark", "Thunder" };
+            string[] electricSupportMoves = { "", "", "" };
         }
+    }
+    void pokedex()
+    {
+        // Have to display this on the screen when asked
+        List<string> pokemonNames = new List<string>(this.pokemon.Keys);
+    }
+    string getImage(String q)
+    {
+        return q + ".png";
     }
 }
