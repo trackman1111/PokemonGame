345d99d1b93da5ccd4870857361e77d8b90ffec5
Colton Matthews <colton.matthews17@gmail.com>
2019-03-02 10:28:30 -0500

    world items remove themselves after collected

--- Pokemon Purple/Assets/ItemClass.cs
+++ Pokemon Purple/Assets/ItemClass.cs
@@ -7,7 +7,6 @@ public class ItemClass : MonoBehaviour
     public Trainer trainer;
     public string name;
  // public Movement movement;
-    int added = 0;
     bool answered;
     string itemName;
     // Start is called before the first frame update
@@ -21,18 +20,20 @@ public class ItemClass : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ( !answered && added == 0 )
+        if ( !answered )
         {
             if (Input.GetKeyDown(KeyCode.Y))
             {
                 trainer.bag.Add(itemName);
                 print("You have added " + name + " to your Bag!");
                 answered = true;
-                added++;
+
+                Destroy(this.gameObject);
             }
             else if (Input.GetKeyDown(KeyCode.N))
             {
                 answered = true;
+                Destroy(this.gameObject);
             }
         }
     }
@@ -40,15 +41,8 @@ public class ItemClass : MonoBehaviour
     public void OnCollisionEnter2D(Collision2D collision)
     {
         FindObjectOfType<Movement>().setStasis(true);
-        if ( added == 0 )
-        {
-            print("Would you like to pickup the " + itemName + "? (Y/N)");
-            answered = false;
-        }
-        else
-        {
-            print("This Pokeball has already been opened!");
-        }
+        print("Would you like to pickup the " + itemName + "? (Y/N)");
+        answered = false;
     }
     public void OnCollisionExit2D(Collision2D collision)
     {
